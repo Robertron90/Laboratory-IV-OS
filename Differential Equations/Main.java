acad0888d0e5c6b1d9f750ebe83823be0d170dfc
Robertron90 <35875418+Robertron90@users.noreply.github.com>
2018-11-02 16:07:38 +0300

    Update Main.java

--- Differential Equations/Main.java
+++ Differential Equations/Main.java
@@ -5,22 +5,17 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.scene.chart.LineChart;
-import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.XYChart;
-
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Equation values");
-        primaryStage.setScene(new Scene(root, 350, 280));
+        primaryStage.setTitle("Passing a values");
+        primaryStage.setScene(new Scene(root, 650, 300));
         primaryStage.show();
     }
+
 
 
     public static void main(String[] args) {
